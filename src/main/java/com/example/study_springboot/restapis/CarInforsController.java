91a3cc2741019d63d0695d008ce50c294da79f4d
SSSanga <ksa901125@gmail.com>
2023-07-03 15:26:31 +0900

    selectAll 까지

--- src/main/java/com/example/study_springboot/restapis/CarInforsController.java
+++ src/main/java/com/example/study_springboot/restapis/CarInforsController.java
@@ -1,11 +1,16 @@
 package com.example.study_springboot.restapis;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.study_springboot.service.CarInforsService;
@@ -16,10 +21,38 @@ public class CarInforsController {
     @Autowired
     CarInforsService carInforsService;
 // /selectDetail/CI002
+
+  @GetMapping("/selectAll/{CAR_INFOR_ID}")
+    // {}변수는 pathVariable로 capping해서 가져올것.
+    public ResponseEntity selectAll(@PathVariable String CAR_INFOR_ID) {
+        Object result = carInforsService.selectAll(CAR_INFOR_ID);
+
+        return ResponseEntity.ok().body(result);
+    }
     @GetMapping("/selectDetail/{CAR_INFOR_ID}")
     // {}변수는 pathVariable로 capping해서 가져올것.
     public ResponseEntity selectDetail(@PathVariable String CAR_INFOR_ID) {
         Object result = carInforsService.selectDetail(CAR_INFOR_ID);
+
+        return ResponseEntity.ok().body(result);
+    }
+    
+    @PostMapping("/insert")
+    // 입력
+    public ResponseEntity insert (@RequestBody Map paramMap){
+        Object result = carInforsService.insert(paramMap);
+        return ResponseEntity.ok().body(result);
+    }
+    @PutMapping("/update")
+    // 업데이트
+    public ResponseEntity updatd (@RequestBody Map paramMap){
+        Object result = carInforsService.update(paramMap);
+        return ResponseEntity.ok().body(result);
+    }
+    @DeleteMapping("/delete/{CAR_INFOR_ID}")
+    // {}변수는 pathVariable로 capping해서 가져올것.
+    public ResponseEntity delete(@PathVariable String CAR_INFOR_ID) {
+        Object result = carInforsService.delete(CAR_INFOR_ID);
 
         return ResponseEntity.ok().body(result);
     }
--- src/main/java/com/example/study_springboot/service/CarInforsService.java
+++ src/main/java/com/example/study_springboot/service/CarInforsService.java
@@ -1,6 +1,7 @@
 package com.example.study_springboot.service;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,16 @@ import com.example.study_springboot.dao.SharedDao;
 public class CarInforsService {
     @Autowired
     SharedDao sharedDao;
+    public Object selectAll(String CAR_INFOR_ID) {
+        String sqlMapId = "CarInfors.selectAll";
+
+        HashMap dataMap = new HashMap<>();
+        dataMap.put("CAR_INFOR_ID", CAR_INFOR_ID);
+
+        Object result = sharedDao.getList(sqlMapId, dataMap);
+        return result;
+
+    }
 
     public Object selectDetail(String CAR_INFOR_ID) {
         // SharedDao에서 호출한다. _GetOne 하나만 호출
@@ -27,4 +38,30 @@ public class CarInforsService {
 
     }
 
+    public Object insert(Map dataMap) {
+        // 입력 or 삽입 in sharedDao
+        // Object result = sqlSessionTemplate.insert(sqlMapId, dataMap);
+        String sqlMapId = "CarInfors.insert";
+        Object result = sharedDao.insert(sqlMapId, dataMap);
+        return result;
+    }
+
+    public Object update(Map dataMap) {
+        // 업데이트 in sharedDao
+        // Object result = sqlSessionTemplate.update(sqlMapId, dataMap);
+        String sqlMapId = "CarInfors.update";
+        Object result = sharedDao.update(sqlMapId, dataMap);
+        return result;
+    }
+
+    public Object delete(String CAR_INFOR_ID) {
+        // Object result = sqlSessionTemplate.delete(sqlMapId, dataMap);
+        String sqlMapId = "CarInfors.delete";
+        HashMap dataMap = new HashMap<>();
+        dataMap.put("CAR_INFOR_ID", CAR_INFOR_ID);
+
+        Object result = sharedDao.delete(sqlMapId, dataMap);
+        // client가 
+        return result;
+    }
 }
